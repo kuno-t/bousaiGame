ee7b4529810c406ea9b5df164d3149a948d8d8d0
kuno-t <71215380+kuno-t@users.noreply.github.com>
2021-02-09 10:03:13 +0900

    Update bousaiGameScript.js

--- bousaiGameScript.js
+++ bousaiGameScript.js
@@ -33,11 +33,25 @@ window.onload = questionChoice;
 
 /* こっちはデバッグ 
 $(function(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'bousaiData.json.js';
-    document.body.appendChild(script);
-    var bousaiJSON = window.DEBUGJSON;
+    var bousaiJSON = {
+        question:[{
+            text:"問１：大きな地震に被災したときのことを想定します。<br>備蓄のあった以下の食べ物のどれかについて、<br>他の選択肢には無い長所を考えてみましょう。<br>限られた条件下で特に有用であれば、<br>条件を付け足してみても構いません。<br>",
+            image:[{
+                alt:"カップラーメン",
+                src:"food_cup_noodle_close.png"
+            },
+            {
+                alt:"乾パン",
+                src:"bousai_kanpan.png"
+            },
+            {
+                alt:"缶詰",
+                src:"bfood_kandume_close.png"
+            }
+            ]
+        }
+        ]
+    }
     Qnum = Math.floor(Math.random()*bousaiJSON.question.length);
     Inum = Math.floor(Math.random()*bousaiJSON.question[Qnum].image.length);
     console.log(bousaiJSON.question.length,"\n",bousaiJSON.question[Qnum].text,"\n",bousaiJSON.question[Qnum].image.length);
